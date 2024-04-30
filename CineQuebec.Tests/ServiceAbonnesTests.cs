89259bc7ed950a3abb3c98ccbb65520718ef8deb
Julien Paquet-Beaudoin <paqj8978@gmail.com>
2024-04-30 15:50:48 -0400

    Fini test

--- CineQuebec.Tests/ServiceAbonnesTests.cs
+++ CineQuebec.Tests/ServiceAbonnesTests.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
-using CineQuebec.Windows.DAL;
+using CineQuebec.Windows.DAL.Interfaces;
+using CineQuebec.Windows.Services;
+using CineQuebec.Windows.Domain;
+using MongoDB.Bson;
 
 namespace CineQuebec.Tests
 {
@@ -13,7 +16,19 @@ namespace CineQuebec.Tests
         [Fact]
         public void UdpdateAbonneShouldReturnUpdatedAbonne()
         {
-            //var mockRepository = new Mock<IAbonneRepository>();
+            // Arrange
+            var mockRepository = new Mock<IRepositoryAbonnes>();
+            var service = new ServiceAbonnes(mockRepository.Object);
+            var abonne = new Mock<IAbonne>();
+            mockRepository.Setup(x => x.UpdateAbonne(abonne.Object)).Returns(abonne.Object);
+
+            //Act
+            var result = service.UpdateAbonne(abonne.Object);
+
+            //Assert
+            Assert.Equal(abonne.Object, result);
+
+
         }
     }
 }
