a4e2f2cdf4b1febe18a9a23ccbe697a26b6f8164
Julien Paquet-Beaudoin <paqj8978@gmail.com>
2024-05-07 15:10:32 -0400

    Ajout Repository pour Recompense

---
+++ CineQuebec.Windows/DAL/Repositories/RepositoryRecompense.cs
@@ -0,0 +1,28 @@
+﻿using CineQuebec.Windows.DAL.Interfaces;
+using CineQuebec.Windows.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.DAL.Repositories
+{
+    public class RepositoryRecompense : GenericRepository<Recompense>, IRepositoryRecompense
+    {
+        public bool AddRecompenseToAbonne(Recompense recompense)
+        {
+            try
+            {
+                Collection.InsertOne(recompense);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Impossible d'ajouter le film " + ex.Message, "Erreur");
+            }
+
+            return false;
+        }
+    }
+}
