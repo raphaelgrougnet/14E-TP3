780f494e6d0edfdcdacbe679f16f0471d56a2f02
Julien Paquet-Beaudoin <paqj8978@gmail.com>
2024-05-07 14:06:43 -0400

    Modif test

--- CineQuebec.Tests/ServiceAbonnesTests.cs
+++ CineQuebec.Tests/ServiceAbonnesTests.cs
@@ -19,7 +19,7 @@ namespace CineQuebec.Tests
             // Arrange
             var mockRepository = new Mock<IRepositoryAbonnes>();
             var service = new ServiceAbonnes(mockRepository.Object);
-            var id = new ObjectId("5f8f4b3b7b3e6f0b7c7b3e6f"); //change this to a real ObjectId
+            var id = new ObjectId("5f8f4b3b7b3e6f0b7c7b3e6f"); 
 
             var preferences = new Preference(id, new List<Acteur>(), new List<Realisateur>(), new List<Directeur>(), new List<EnumCategorie>());
             preferences.Acteurs.Add(new Acteur("Albert"));
