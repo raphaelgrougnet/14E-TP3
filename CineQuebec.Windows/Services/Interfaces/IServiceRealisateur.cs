f5e924fc083dbeef27d046478279d437aaeaaa04
Julien Paquet-Beaudoin <paqj8978@gmail.com>
2024-05-07 14:07:31 -0400

    Ajout des interfaces de services

---
+++ CineQuebec.Windows/Services/Interfaces/IServiceRealisateur.cs
@@ -0,0 +1,16 @@
+﻿using CineQuebec.Windows.Domain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.Services.Interfaces
+{
+    public interface IServiceRealisateur
+    {
+        public ReadOnlyCollection<Realisateur> LoadRealisateurs();
+
+    }
+}
