dc08fb95963bb1cd999320b8006c028064ea0965
Julien Paquet-Beaudoin <paqj8978@gmail.com>
2024-05-09 12:51:45 -0400

    Ajouter test + modif des test précédent

--- CineQuebec.Tests/ServiceRecompenseTests.cs
+++ CineQuebec.Tests/ServiceRecompenseTests.cs
@@ -16,7 +16,7 @@ namespace CineQuebec.Tests
             var mockRepository = new Mock<IRepositoryRecompense>();
             var serviceRecompense = new ServiceRecompense(mockRepository.Object);
             var id = new ObjectId("5f8f4b3b7b3e6f0b7c7b3e6f");
-            var recompense = new Recompense(ObjectId.GenerateNewId(), "Test", EnumType.TicketGratuit, "Merci de votre participation", id);
+            var recompense = new Recompense(ObjectId.GenerateNewId(), "Test", EnumType.TicketGratuit, "Merci de votre participation", 30 ,id);
 
             mockRepository.Setup(x => x.AddRecompenseToAbonne(recompense)).Returns(true);
 
@@ -34,7 +34,7 @@ namespace CineQuebec.Tests
             var mockRepository = new Mock<IRepositoryRecompense>();
             var serviceRecompense = new ServiceRecompense(mockRepository.Object);
             var id = new ObjectId("5f8f4b3b7b3e6f0b7c7b3e6f");
-            var recompense = new Recompense(ObjectId.GenerateNewId(), "Test", EnumType.TicketGratuit, "Merci de votre participation", id);
+            var recompense = new Recompense(ObjectId.GenerateNewId(), "Test", EnumType.TicketGratuit, "Merci de votre participation", 30, id);
 
             mockRepository.Setup(x => x.AddRecompenseToAbonne(recompense)).Returns(false);
 
@@ -52,12 +52,42 @@ namespace CineQuebec.Tests
             var mockRepository = new Mock<IRepositoryRecompense>();
             var serviceRecompense = new ServiceRecompense(mockRepository.Object);
             var id = new ObjectId("5f8f4b3b7b3e6f0b7c7b3e6f");
-            var recompense = new Recompense(ObjectId.GenerateNewId(), "Test", EnumType.TicketGratuit, "Merci de votre participation", id);
+            var recompense = new Recompense(ObjectId.GenerateNewId(), "Test", EnumType.TicketGratuit, "Merci de votre participation", 30, id);
 
             mockRepository.Setup(x => x.AddRecompenseToAbonne(recompense)).Throws<Exception>();
 
             // Act & Assert
             Assert.Throws<Exception>(() => serviceRecompense.AddRecompenseToAbonne(recompense));
+        }
+
+        [Fact]
+        public void TestDecrementerNombreRestant_WithPositiveNumber()
+        {
+            // Arrange
+            var id = new ObjectId("5f8f4b3b7b3e6f0b7c7b3e6f");
+            var recompense = new Recompense("Test", EnumType.TicketGratuit, "Test Description", 5, id);
+
+            // Act
+            var result = recompense.DecrementerNombreRestant();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal((uint)4, recompense.NombreRestant);
+        }
+
+        [Fact]
+        public void TestDecrementerNombreRestant_WithZero()
+        {
+            // Arrange
+            var id = new ObjectId("5f8f4b3b7b3e6f0b7c7b3e6f");
+            var recompense = new Recompense("Test", EnumType.TicketGratuit, "Test Description", 0, id);
+
+            // Act
+            var result = recompense.DecrementerNombreRestant();
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal((uint)0, recompense.NombreRestant);
         }
     }
 }
