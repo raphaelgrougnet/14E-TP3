98e7fd747348aef9c5f6bcb0eab87270ecd9f624
Raphaël Grougnet <ragro001@edu.cegepgarneau.ca>
2024-05-16 16:49:37 -0400

    truc la

--- CineQuebec.Windows/View/AdminAbonnesControl.xaml.cs
+++ CineQuebec.Windows/View/AdminAbonnesControl.xaml.cs
@@ -41,33 +41,6 @@ namespace CineQuebec.Windows.View
             } 
         }
 
-        private void OffrirRecompense(Abonne abonne)
-        {
-            try
-            {
-                Abonne abonneSelectionne = abonne;
-                MessageBoxResult reponse = MessageBox.Show("Voulez-vous offrir une récompense à cet abonné ?", 
-                    "Récompense", MessageBoxButton.YesNo, MessageBoxImage.Question);
-                if (reponse == MessageBoxResult.Yes)
-                {
-                    MessageBox.Show("Félicitation vous avez offert une récompense à " + abonne.Nom + " " + abonne.Prenom, 
-                        "Récompense", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Une erreur est survenue lors de la distribution de la récompense" + ex.Message, "Erreur");
-            }           
-        }
-        
-        private void lstAbonnes_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (lstAbonnes.SelectedIndex != -1)
-            {
-                OffrirRecompense(lstAbonnes.SelectedItem as Abonne);
-            }
-        }
-
         private void btnRetour_Click(object sender, RoutedEventArgs e)
         {
             ((MainWindow)Application.Current.MainWindow).AdminHomeControl();
